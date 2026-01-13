c896279651618f6a23f4b68f37de5054e8c2706d
Lukas Baur <lukas.baur@ipa.fraunhofer.de>
2026-01-13 13:20:27 +0100

    Placeholde code for first analysis tool

--- analysis/self_similarity.py
+++ analysis/self_similarity.py
@@ -1,11 +1,27 @@
 # TODO LSB
-from turtle import pd
+import pandas as pd
 from typing import List
 import matplotlib.pyplot as plt
 
-def generate_self_similary_plot(data_df:  pd.DataFrame, benchmark_columns: List[str], target_column: str):
+def generate_self_similary_plot(data_df: pd.DataFrame, reference_columns: List[str], target_column: str):
     fig = plt.figure(figsize=(20, 10))
-    plt.plot([1,2,3], [4,5,6], label='nice one!')
+    plt.plot([1,2,3,4], [4,5,8,5], label='nice one!')
 
     return fig
 
+
+
+if __name__ == '__main__':
+    # test functionality
+    import pandas as pd
+    import numpy as np
+    from constants import DEFAULT_SAMPLE_FILE
+
+    df = pd.read_csv(DEFAULT_SAMPLE_FILE)
+    print(df.head())
+
+    target_column = "load_665"
+    reference_columns = [r"load_{id}" for id in [116, 230, 640, ]] # ["load_116", "load_230", "load_640"]
+
+    plt_instance = generate_self_similary_plot(df, reference_columns, 'load_665')
+    plt.show()
