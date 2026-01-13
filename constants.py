2770fcc2727aaa4df194668ba71914ced6d01913
Lukas Baur <lukas.baur@ipa.fraunhofer.de>
2026-01-13 13:04:33 +0100

    Constant.py added + placeholder for frontend

--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
 GLOBAL_DATA_PATH = "D:\EnerBench"
 
 # a small dataset to play with
-DEFAULT_SAMPLE_FILE = r"{GLOBAL_DATA_PATH}\RawData\default_sample_file.csv"
+#DEFAULT_SAMPLE_FILE = r"{GLOBAL_DATA_PATH}\RawData\default_sample_file.csv"
+DEFAULT_SAMPLE_FILE = r"https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
+
