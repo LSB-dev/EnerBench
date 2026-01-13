6d26100ca9f4c5593da75eddf3d474036c3445bd
Lukas Baur <lukas.baur@ipa.fraunhofer.de>
2026-01-13 14:31:31 +0100

    Updating .gitignore

--- analysis/self_similarity/run_self_similarity.py
+++ analysis/self_similarity/run_self_similarity.py
@@ -8,9 +8,9 @@ from analysis.self_similarity.metrics import evaluation_metrics
 from shared_util import check_all_columns_are_in_df
 
 
-def _lag_1d_similarity(df: pd.DataFrame, metric="sMAPE"):
-    shifted_df = df.shift(96)[96:]
-    original_df = df[96:]
+def _lag_similarity(df: pd.DataFrame, metric="sMAPE", lags=96):
+    shifted_df = df.shift(lags)[lags:]
+    original_df = df[lags:]
     assert metric in evaluation_metrics
     metric_func = evaluation_metrics[metric]
     scores = {
@@ -19,11 +19,18 @@ def _lag_1d_similarity(df: pd.DataFrame, metric="sMAPE"):
     return scores
 
 
+def _lag_1d_similarity(df: pd.DataFrame, metric="sMAPE"):
+    return _lag_similarity(df, metric=metric, lags=96)
+
+
+def _lag_1w_similarity(df: pd.DataFrame, metric="sMAPE"):
+    return _lag_similarity(df, metric=metric, lags=96 * 7)
+
+
 def check_all_columns_have_no_nan(data_df, reference_columns, target_column):
     all_cols = reference_columns + [target_column]
     for i, (col_hasna, col) in enumerate(zip(data_df[all_cols].isna().any(axis=0), all_cols)):
         assert not col_hasna, f"Column '{col}' has missing values: {data_df[col].isna().sum()}"
-
 
 
 def generate_self_similarity_plot(data_df: pd.DataFrame, reference_columns: List[str], target_column: str):
@@ -33,12 +40,16 @@ def generate_self_similarity_plot(data_df: pd.DataFrame, reference_columns: List
 
     # do some work
     lag_1_scores = _lag_1d_similarity(data_df[reference_columns + [target_column]])
+    lag_7_scores = _lag_1w_similarity(data_df[reference_columns + [target_column]])
     print(lag_1_scores)
 
+    best_lag_scores = {col: min(lag_1_scores[col], lag_7_scores[col]) for col in lag_1_scores}
+
     # create and return result plot
-    fig = plt.figure(figsize=(20, 10))
-    plt.hist( [lag_1_scores[key] for key in reference_columns], label='Scores (the smaller the better)')
-    plt.vlines(x=lag_1_scores[target_column], ymin=0, ymax=1, colors='r', label='Target-Column')
+    fig = plt.figure(figsize=(10, 10))
+    plt.hist([lag_1_scores[key] for key in reference_columns], label='Similarity Scores (the smaller the more similar)')
+    plt.vlines(x=lag_1_scores[target_column], ymin=0, ymax=1, colors='r', label='Target')
+    plt.legend()
     return fig
 
 
